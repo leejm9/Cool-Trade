a44dd0887e00613da577e150058fdd24a014f532
(이종민) <(jung9811002@naver.com)>
2024-03-05 09:03:39 +0900

    수정

--- SemiProject_CoolTrade/src/main/java/com/cooltrade/notice/controller/NoticeDeleteController.java
+++ SemiProject_CoolTrade/src/main/java/com/cooltrade/notice/controller/NoticeDeleteController.java
@@ -14,13 +14,13 @@ import com.cooltrade.notice.model.service.NoticeService;
  * Servlet implementation class noticeDeleteController
  */
 @WebServlet("/delete.no")
-public class noticeDeleteController extends HttpServlet {
+public class NoticeDeleteController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public noticeDeleteController() {
+    public NoticeDeleteController() {
         super();
         // TODO Auto-generated constructor stub
     }
